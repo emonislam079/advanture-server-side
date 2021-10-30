34c90b3a94c8e25199ee2d05d3bf439116d4f5fc
emonislam079 <emonislam079@gmail.com>
2021-10-30 22:22:38 +0600

    single data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -29,12 +30,18 @@ async function run(){
         const cursor = toursCollection.find({});
         const Tour= await cursor.toArray();
         res.send(Tour);
+    })  
+    
+    // get single data
 
-
-    })   
-
-
-
+    app.get('/Tours/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const Tour = await toursCollection.findOne(query);
+        
+        res.send(Tour);
+    })
+    
     }
     finally{
         // await client.close();
